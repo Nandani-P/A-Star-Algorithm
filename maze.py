d96f24b295168de7f8cad2f34fb14f371e080908
Nandani <np744@scarletmail.rutgers.edu>
2020-10-04 10:48:09 -0400

    Final version of maze generator

--- maze.py
+++ maze.py
@@ -1,24 +1,19 @@
+#
+# Author: Nandani Patidar
+#
+# AI Project One
+#
+
 from random import randrange, shuffle
 import json
-class Maze(object):
-    
-    """def makeMaze(self, dimension):
-                
-        for x in range(dimension):
-            row = []
-            for y in range(dimension):                  
-                row.append(randrange(2))
-                
-            shuffle(row)
-            self.grid.append(row)
-        return self.grid"""
+class Maze(object):    
     
     def makeMaze(self, dimension):
         grid = []        
         for x in range(dimension):
             row = []
             for y in range(dimension):                  
-                if randrange(10) < 7:
+                if randrange(10) < 7:  
                     row.append(1)
                 else:
                     row.append(0)
@@ -29,8 +24,7 @@ class Maze(object):
 
          
         
-    def displayMaze(self, arryMaze):
-        
+    def displayMaze(self, arryMaze):      
         for a in arryMaze:
             counter =0
             for i in a:
@@ -47,39 +41,16 @@ class Maze(object):
                 counter = counter + 1
             print("---------------------")
         
-
-    '''def mulGrid(self, num, dimension):
-        file = open("gridStore.txt", "w+")
-        for i in range(num):
-            file.write(str(self.makeMaze(dimension)))
-            file.write('\n')
-        file.close()
-
-        gridOne = []
-
-        with open('gridStore.txt', 'r') as file:
-            for line in file:
-                item = line[:-1]
-                gridOne.append(item)
-
-   def mulGrid(self, dimension):
-        with open('listfile.txt', 'w') as file:
-            json.dump(self.makeMaze(dimension), file)
-
-        with open('listfile.txt', 'r') as file:
-            basicList = json.load(file)
-        print(type(basicList))
-        return basicList'''
     
     def mulGrid(self, num, dimension):
         
-        with open('listfile.txt', 'w') as file:
+        with open('storeMaze.txt', 'w') as file:
             for i in range(num):
                 x = json.dumps(self.makeMaze(dimension))
                 file.write(x + "\n")
                 file.flush()
 
-        with open('listfile.txt', 'r') as file:
+        with open('storeMaze.txt', 'r') as file:
             y = file.readlines()
             arryMaze = []
             for i in y:
@@ -88,12 +59,8 @@ class Maze(object):
         return arryMaze
                 
                 
-
-
         
-a = Maze()
-    
+a = Maze()    
 #a.makeMaze(5, 5)
-
 #a.mulGrid(5, 5)
 a.displayMaze(a.mulGrid(5, 5))
